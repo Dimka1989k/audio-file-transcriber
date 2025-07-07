39d1ae68b6b8519079c920114e03ae7430ffb3f4
Dmytro Kyselov <dima1989k@ukr.net>
2025-07-07 13:52:30 +0300

    change search params

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -1,20 +1,26 @@
 import { redirect } from 'next/navigation';
 import { stripe } from '../../lib/stripe';
 
-export default async function Success({ searchParams }: { searchParams: URLSearchParams }) {
-  const session_id = searchParams.get('session_id');
+export default async function Success({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
+  const session_id = Array.isArray(searchParams.session_id)
+    ? searchParams.session_id[0]
+    : searchParams.session_id;
 
   if (!session_id)
     throw new Error('Please provide a valid session_id (`cs_test_...`)');
 
- const {
-  status,
-  customer_details,
-} = await stripe.checkout.sessions.retrieve(session_id, {
-  expand: ['line_items', 'payment_intent'],
-});
+  const {
+    status,
+    customer_details,
+  } = await stripe.checkout.sessions.retrieve(session_id, {
+    expand: ['line_items', 'payment_intent'],
+  });
 
-const customerEmail = customer_details?.email ?? 'Unknown email';
+  const customerEmail = customer_details?.email ?? 'Unknown email';
 
   if (status === 'open') {
     redirect('/');
