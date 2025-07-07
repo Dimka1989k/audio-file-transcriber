5af84f9cf45ae2d03eb766509052e7dc29e00521
Dmytro Kyselov <dima1989k@ukr.net>
2025-07-07 15:25:41 +0300

    add logic

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -1,21 +1,18 @@
 import { redirect } from "next/navigation";
 import { stripe } from "../../lib/stripe";
 
-export default async function SuccessPage({
-  searchParams,
-}: {
+interface SuccessPageProps {
   searchParams: { session_id?: string };
-}) {
+}
+
+export default async function SuccessPage({ searchParams }: SuccessPageProps) {
   const session_id = searchParams.session_id;
 
   if (!session_id) {
     throw new Error("Missing session_id in searchParams");
   }
 
-  const {
-    status,
-    customer_details,
-  } = await stripe.checkout.sessions.retrieve(session_id, {
+  const { status, customer_details } = await stripe.checkout.sessions.retrieve(session_id, {
     expand: ["line_items", "payment_intent"],
   });
 
@@ -29,8 +26,8 @@ export default async function SuccessPage({
     return (
       <section id="success">
         <p>
-          We appreciate your business! A confirmation email will be sent to{" "}
-          {customerEmail}. If you have any questions, please email
+          We appreciate your business! A confirmation email will be sent to {customerEmail}.
+          If you have any questions, please email
         </p>
         <a href="mailto:orders@example.com">orders@example.com</a>.
       </section>
